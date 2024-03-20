1b003bc914c7e87241373a8282eb7ba5f1826ed6
shawky  sobhy <shawkysobhy98@gmail.com>
2024-03-20 02:04:39 +0200

    update header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,12 +6,12 @@ function Header() {
 	return (
 		<header className='h-[81px] px-[1rem] sm:px-[3.6rem] flex items-center justify-between sticky top-0 z-20 bg-bgOpaque  text-md backdrop-blur-md'>
 			<Social />
-			<a
+			{/* <a
 				href={CONTACTS.ruesme}
 				target='_blank'
 				className='px-4 py-2  md:px-8  text-[16px] md:text-sm font-semibold font-titilllium text-gray-200 transition-colors duration-200 border outline-none cursor-pointer bg-emerald-800 rounded-md border-mintgreen hover:opacity-80 flex items-center justify-center space-x-4'>
 				<span>My resume</span> <FaGoogleDrive />
-			</a>
+			</a> */}
 		</header>
 	);
 }
